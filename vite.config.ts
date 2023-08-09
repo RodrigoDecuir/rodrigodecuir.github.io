1b3ba22faf9fbef6c0f1a65414eb552228bdad6b
Rodrigo Andre Decuir Fuentes <ojo@ciencias.unam.mx>
2023-08-09 15:41:12 -0600

    deployyyy

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
-	build: {
-		outDir: './build' // relative to index.html
-		// emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
-	}
+	plugins: [sveltekit()]
+	//build: {
+	//	outDir: './build' // relative to index.html
+	// emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
+	//}
 };
 
 export default config;
