67d2cad9b6d6b5f25e297dabed6b7515c243a457
Rodrigo Andre Decuir Fuentes <ojo@ciencias.unam.mx>
2023-08-10 09:34:43 -0600

    Point ALIAS, ANAME, or flattened CNAME record to apex-loadbalancer.netlify.com

--- .svelte-kit/output/client/_app/immutable/chunks/paths.d264aea3.js
+++
@@ -1,1 +0,0 @@
-var s;const i=((s=globalThis.__sveltekit_zgi3zr)==null?void 0:s.base)??"/rodrigodecuir.github.io";var e;const t=((e=globalThis.__sveltekit_zgi3zr)==null?void 0:e.assets)??i;export{t as a,i as b};
--- .svelte-kit/output/server/nodes/0.js
+++ .svelte-kit/output/server/nodes/0.js
@@ -3,6 +3,6 @@
 export const index = 0;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.0372a5b7.js","_app/immutable/chunks/scheduler.3fb37d42.js","_app/immutable/chunks/index.d2400dde.js","_app/immutable/chunks/paths.d264aea3.js"];
+export const imports = ["_app/immutable/nodes/0.1e8d5070.js","_app/immutable/chunks/scheduler.3fb37d42.js","_app/immutable/chunks/index.d2400dde.js","_app/immutable/chunks/paths.786337de.js"];
 export const stylesheets = ["_app/immutable/assets/0.08e8f34d.css"];
 export const fonts = [];
