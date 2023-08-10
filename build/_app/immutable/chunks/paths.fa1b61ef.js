f59d0ca87f4321bd4c6cec3189571de4afc0a46a
Rodrigo Andre Decuir Fuentes <ojo@ciencias.unam.mx>
2023-08-09 21:35:05 -0600

    deploy with netlify

--- build/_app/immutable/chunks/paths.fa1b61ef.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_hd80b4)==null?void 0:s.base)??"/rodrigodecuir.github.io";var e;const a=((e=globalThis.__sveltekit_hd80b4)==null?void 0:e.assets)??t;export{a,t as b};
