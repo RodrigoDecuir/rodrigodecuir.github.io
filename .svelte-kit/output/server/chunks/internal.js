f8b414277ac8d2bca8021cd5c142d09852017653
Rodrigo Andre Decuir Fuentes <ojo@ciencias.unam.mx>
2023-08-09 18:42:03 -0600

    deploy AZURE

--- .svelte-kit/output/server/chunks/internal.js
+++ .svelte-kit/output/server/chunks/internal.js
@@ -1,5 +1,5 @@
 import { c as create_ssr_component, a as setContext, v as validate_component, m as missing_component } from "./ssr.js";
-let base = "";
+let base = "/rodrigodecuir.github.io";
 let assets = base;
 const initial = { base, assets };
 function reset() {
@@ -166,7 +166,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "lmtw6y"
+  version_hash: "hd80b4"
 };
 function get_hooks() {
   return {};
