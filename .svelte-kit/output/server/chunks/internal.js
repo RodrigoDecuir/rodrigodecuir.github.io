6afeff7be1a97bda30fb95d97d4b4c7df58ec347
Rodrigo Andre Decuir Fuentes <ojo@ciencias.unam.mx>
2023-11-26 10:34:53 -0600

    fix: link that was not working

--- .svelte-kit/output/server/chunks/internal.js
+++ .svelte-kit/output/server/chunks/internal.js
@@ -193,7 +193,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "zql7v9"
+  version_hash: "12y7eds"
 };
 function get_hooks() {
   return {};
