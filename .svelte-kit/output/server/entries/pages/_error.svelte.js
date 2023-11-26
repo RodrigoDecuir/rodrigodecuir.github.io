691f1aef68c3dd9eebd3d563a0822bf81bf276b7
Rodrigo Andre Decuir Fuentes <ojo@ciencias.unam.mx>
2023-11-25 19:40:21 -0600

    fixed untracked files

--- .svelte-kit/output/server/entries/pages/_error.svelte.js
+++
@@ -1,7 +0,0 @@
-import { c as create_ssr_component } from "../../chunks/ssr.js";
-const Error = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<div class="text-center p-12 manrope" data-svelte-h="svelte-9thzhc"><h1 class="text-4xl italic mb-auto"><span class="text-red-800">err</span>or 404</h1> <a href="/" class="text-blue-700 hover:text-blue-500 transition-colors text-lg">return home</a></div>`;
-});
-export {
-  Error as default
-};
--- .svelte-kit/output/server/nodes/10.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 10;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/(app)/writing/(sketch)/(s-01)/_layout@.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/10.222015c6.js","_app/immutable/chunks/scheduler.762b4b31.js","_app/immutable/chunks/index.19b44a2f.js"];
-export const stylesheets = [];
-export const fonts = [];
